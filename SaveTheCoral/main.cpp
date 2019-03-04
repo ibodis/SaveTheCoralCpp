05d72a4d7e7de6300eefae555b2cd185fe169437
ibodis <ibodis@hotmail.com>
2019-03-03 19:55:59 -0800

    Added molecule speed

--- SaveTheCoral/main.cpp
+++ SaveTheCoral/main.cpp
@@ -33,7 +33,6 @@ sf::RectangleShape coral;
 // Universal shape properties
 float shapeOutlineThickness = 1;
 sf::Color shapeOutlineColor(100, 100, 200);
-int movementAmount = 4;
 sf::Color textColor(sf::Color::Black);
 
 // Water properties
@@ -42,42 +41,47 @@ sf::Color waterColor(32, 178, 170);
 // Carbon dioxide properties
 const int MIN_CarbonDioxideCount = 10;
 const int MAX_CarbonDioxideCount = 300;
-sf::CircleShape CarbonDioxideMolecules[MAX_CarbonDioxideCount];
-int currentCarbonDioxideCount = MIN_CarbonDioxideCount;
 sf::Color carbonDioxideColor(107, 142, 35);
+sf::CircleShape carbonDioxideMolecules[MAX_CarbonDioxideCount];
+int currentCarbonDioxideCount = MIN_CarbonDioxideCount;
+int carbonDioxideSpeed = 6;
 float carbonDioxideSize = 4;
 
 // Carbonic acid properties
 const int MIN_CarbonicAcidCount = 10;
 const int MAX_CarbonicAcidCount = 300;
 sf::Color carbonicAcidColor(255, 165, 0);
-sf::CircleShape CarbonicAcidMolecules[MAX_CarbonicAcidCount];
+sf::CircleShape carbonicAcidMolecules[MAX_CarbonicAcidCount];
 int currentCarbonicAcidCount = MIN_CarbonicAcidCount;
+int carbonicAcidSpeed = 4;
 float carbonicAcidSize = 6.5;
 
 // Carbonate properties
-sf::Color carbonateColor(255, 160, 122);
-float carbonateSize = 6;
 const int MIN_CarbonateCount = 10;
 const int MAX_CarbonateCount = 250;
-sf::CircleShape CarbonateMolecules[MAX_CarbonateCount];
+sf::Color carbonateColor(255, 160, 122);
+sf::CircleShape carbonateMolecules[MAX_CarbonateCount];
 int currentCarbonateCount = MIN_CarbonateCount;
+int carbonateSpeed = 4;
+float carbonateSize = 6;
 
 // Bi-carbonate properties
-sf::Color biCarbonateColor(25, 25, 112);
-float biCarbonateSize = 6;
 const int MIN_biCarbonateCount = 10;
 const int MAX_biCarbonateCount = 250;
+sf::Color biCarbonateColor(25, 25, 112);
 sf::CircleShape biCarbonateMolecules[MAX_biCarbonateCount];
 int currentBiCarbonateCount = MIN_biCarbonateCount;
+int biCarbonateSpeed = 4;
+float biCarbonateSize = 6;
 
 // Calcium carbonate properties
-sf::Color calciumCarbonateColor(255, 248, 220);
-float calciumCarbonateSize = 10;
 const int MIN_CalciumCarbonateCount = 10;
 const int MAX_CalciumCarbonateCount = 250;
-sf::CircleShape CalciumCarbonateMolecules[MAX_CalciumCarbonateCount];
+sf::Color calciumCarbonateColor(255, 248, 220);
+sf::CircleShape calciumCarbonateMolecules[MAX_CalciumCarbonateCount];
 int currentCalciumCarbonateCount = MIN_CalciumCarbonateCount;
+int calciumCarbonateSpeed = 2;
+float calciumCarbonateSize = 10;
 
 void InitializeMolecules(Molecule type)
 {
@@ -89,11 +93,11 @@ void InitializeMolecules(Molecule type)
             float x = std::rand() % (int) waterRect.width;
             float y = std::rand() % (int) waterRect.height;
 
-            CarbonDioxideMolecules[i].setPosition(x, waterRect.top + y);
-            CarbonDioxideMolecules[i].setFillColor(carbonDioxideColor);
-            CarbonDioxideMolecules[i].setRadius(carbonDioxideSize);
-            CarbonDioxideMolecules[i].setOutlineThickness(shapeOutlineThickness);
-            CarbonDioxideMolecules[i].setOutlineColor(shapeOutlineColor);
+            carbonDioxideMolecules[i].setPosition(x, waterRect.top + y);
+            carbonDioxideMolecules[i].setFillColor(carbonDioxideColor);
+            carbonDioxideMolecules[i].setRadius(carbonDioxideSize);
+            carbonDioxideMolecules[i].setOutlineThickness(shapeOutlineThickness);
+            carbonDioxideMolecules[i].setOutlineColor(shapeOutlineColor);
         }
         break;
     case CarbonicAcid:
@@ -102,11 +106,11 @@ void InitializeMolecules(Molecule type)
             float x = std::rand() % (int)waterRect.width;
             float y = std::rand() % (int)waterRect.height;
 
-            CarbonicAcidMolecules[i].setPosition(x, waterRect.top + y);
-            CarbonicAcidMolecules[i].setFillColor(carbonicAcidColor);
-            CarbonicAcidMolecules[i].setRadius(carbonicAcidSize);
-            CarbonicAcidMolecules[i].setOutlineThickness(shapeOutlineThickness);
-            CarbonicAcidMolecules[i].setOutlineColor(shapeOutlineColor);   
+            carbonicAcidMolecules[i].setPosition(x, waterRect.top + y);
+            carbonicAcidMolecules[i].setFillColor(carbonicAcidColor);
+            carbonicAcidMolecules[i].setRadius(carbonicAcidSize);
+            carbonicAcidMolecules[i].setOutlineThickness(shapeOutlineThickness);
+            carbonicAcidMolecules[i].setOutlineColor(shapeOutlineColor);   
         }
         break;
     case Carbonate:
@@ -115,11 +119,11 @@ void InitializeMolecules(Molecule type)
             float x = std::rand() % (int)waterRect.width;
             float y = std::rand() % (int)waterRect.height;
 
-            CarbonateMolecules[i].setPosition(x, waterRect.top + y);
-            CarbonateMolecules[i].setFillColor(carbonateColor);
-            CarbonateMolecules[i].setRadius(carbonateSize);
-            CarbonateMolecules[i].setOutlineThickness(shapeOutlineThickness);
-            CarbonateMolecules[i].setOutlineColor(shapeOutlineColor);
+            carbonateMolecules[i].setPosition(x, waterRect.top + y);
+            carbonateMolecules[i].setFillColor(carbonateColor);
+            carbonateMolecules[i].setRadius(carbonateSize);
+            carbonateMolecules[i].setOutlineThickness(shapeOutlineThickness);
+            carbonateMolecules[i].setOutlineColor(shapeOutlineColor);
         }
         break;
     case BiCarbonate:
@@ -141,11 +145,11 @@ void InitializeMolecules(Molecule type)
             float x = std::rand() % (int)waterRect.width;
             float y = std::rand() % (int)waterRect.height;
 
-            CalciumCarbonateMolecules[i].setPosition(x, waterRect.top + y);
-            CalciumCarbonateMolecules[i].setFillColor(calciumCarbonateColor);
-            CalciumCarbonateMolecules[i].setRadius(calciumCarbonateSize);
-            CalciumCarbonateMolecules[i].setOutlineThickness(shapeOutlineThickness);
-            CalciumCarbonateMolecules[i].setOutlineColor(shapeOutlineColor);
+            calciumCarbonateMolecules[i].setPosition(x, waterRect.top + y);
+            calciumCarbonateMolecules[i].setFillColor(calciumCarbonateColor);
+            calciumCarbonateMolecules[i].setRadius(calciumCarbonateSize);
+            calciumCarbonateMolecules[i].setOutlineThickness(shapeOutlineThickness);
+            calciumCarbonateMolecules[i].setOutlineColor(shapeOutlineColor);
         }
         break;
     }
@@ -158,49 +162,49 @@ void AnimateMolecules(Molecule type)
     case CarbonDioxide:
         for (int i = 0; i < currentCarbonDioxideCount; i++)
         {
-            sf::Vector2f currentPosition = CarbonDioxideMolecules[i].getPosition();
+            sf::Vector2f currentPosition = carbonDioxideMolecules[i].getPosition();
 
-            currentPosition.x += (std::rand() % 2 == 0 ? -1 : 1) * (std::rand() % movementAmount);
-            currentPosition.y += (std::rand() % 2 == 0 ? -1 : 1) * (std::rand() % movementAmount);
+            currentPosition.x += (std::rand() % 2 == 0 ? -1 : 1) * (std::rand() % carbonDioxideSpeed);
+            currentPosition.y += (std::rand() % 2 == 0 ? -1 : 1) * (std::rand() % carbonDioxideSpeed);
 
             currentPosition.x = std::min(currentPosition.x, waterRect.left + waterRect.width);
             currentPosition.y = std::min(currentPosition.y, waterRect.top + waterRect.height);
             currentPosition.x = std::max(currentPosition.x, waterRect.left);
             currentPosition.y = std::max(currentPosition.y, waterRect.top);
 
-            CarbonDioxideMolecules[i].setPosition(currentPosition);
+            carbonDioxideMolecules[i].setPosition(currentPosition);
         }
         break;
     case CarbonicAcid:
         for (int i = 0; i < currentCarbonicAcidCount; i++)
         {
-            sf::Vector2f currentPosition = CarbonicAcidMolecules[i].getPosition();
+            sf::Vector2f currentPosition = carbonicAcidMolecules[i].getPosition();
 
-            currentPosition.x += (std::rand() % 2 == 0 ? -1 : 1) * (std::rand() % movementAmount);
-            currentPosition.y += (std::rand() % 2 == 0 ? -1 : 1) * (std::rand() % movementAmount);
+            currentPosition.x += (std::rand() % 2 == 0 ? -1 : 1) * (std::rand() % carbonicAcidSpeed);
+            currentPosition.y += (std::rand() % 2 == 0 ? -1 : 1) * (std::rand() % carbonicAcidSpeed);
 
             currentPosition.x = std::min(currentPosition.x, waterRect.left + waterRect.width);
             currentPosition.y = std::min(currentPosition.y, waterRect.top + waterRect.height);
             currentPosition.x = std::max(currentPosition.x, waterRect.left);
             currentPosition.y = std::max(currentPosition.y, waterRect.top);
 
-            CarbonicAcidMolecules[i].setPosition(currentPosition);
+            carbonicAcidMolecules[i].setPosition(currentPosition);
         }
         break;
     case Carbonate:
         for (int i = 0; i < currentCarbonateCount; i++)
         {
-            sf::Vector2f currentPosition = CarbonateMolecules[i].getPosition();
+            sf::Vector2f currentPosition = carbonateMolecules[i].getPosition();
 
-            currentPosition.x += (std::rand() % 2 == 0 ? -1 : 1) * (std::rand() % movementAmount);
-            currentPosition.y += (std::rand() % 2 == 0 ? -1 : 1) * (std::rand() % movementAmount);
+            currentPosition.x += (std::rand() % 2 == 0 ? -1 : 1) * (std::rand() % carbonateSpeed);
+            currentPosition.y += (std::rand() % 2 == 0 ? -1 : 1) * (std::rand() % carbonateSpeed);
 
             currentPosition.x = std::min(currentPosition.x, waterRect.left + waterRect.width);
             currentPosition.y = std::min(currentPosition.y, waterRect.top + waterRect.height);
             currentPosition.x = std::max(currentPosition.x, waterRect.left);
             currentPosition.y = std::max(currentPosition.y, waterRect.top);
 
-            CarbonateMolecules[i].setPosition(currentPosition);
+            carbonateMolecules[i].setPosition(currentPosition);
         }
         break;
     case BiCarbonate:
@@ -208,8 +212,8 @@ void AnimateMolecules(Molecule type)
         {
             sf::Vector2f currentPosition = biCarbonateMolecules[i].getPosition();
 
-            currentPosition.x += (std::rand() % 2 == 0 ? -1 : 1) * (std::rand() % movementAmount);
-            currentPosition.y += (std::rand() % 2 == 0 ? -1 : 1) * (std::rand() % movementAmount);
+            currentPosition.x += (std::rand() % 2 == 0 ? -1 : 1) * (std::rand() % biCarbonateSpeed);
+            currentPosition.y += (std::rand() % 2 == 0 ? -1 : 1) * (std::rand() % biCarbonateSpeed);
 
             currentPosition.x = std::min(currentPosition.x, waterRect.left + waterRect.width);
             currentPosition.y = std::min(currentPosition.y, waterRect.top + waterRect.height);
@@ -222,17 +226,17 @@ void AnimateMolecules(Molecule type)
     case CalciumCarbonate:
         for (int i = 0; i < currentCalciumCarbonateCount; i++)
         {
-            sf::Vector2f currentPosition = CalciumCarbonateMolecules[i].getPosition();
+            sf::Vector2f currentPosition = calciumCarbonateMolecules[i].getPosition();
 
-            currentPosition.x += (std::rand() % 2 == 0 ? -1 : 1) * (std::rand() % movementAmount);
-            currentPosition.y += (std::rand() % 2 == 0 ? -1 : 1) * (std::rand() % movementAmount);
+            currentPosition.x += (std::rand() % 2 == 0 ? -1 : 1) * (std::rand() % calciumCarbonateSpeed);
+            currentPosition.y += (std::rand() % 2 == 0 ? -1 : 1) * (std::rand() % calciumCarbonateSpeed);
 
             currentPosition.x = std::min(currentPosition.x, waterRect.left + waterRect.width);
             currentPosition.y = std::min(currentPosition.y, waterRect.top + waterRect.height);
             currentPosition.x = std::max(currentPosition.x, waterRect.left);
             currentPosition.y = std::max(currentPosition.y, waterRect.top);
 
-           CalciumCarbonateMolecules[i].setPosition(currentPosition);
+           calciumCarbonateMolecules[i].setPosition(currentPosition);
         }
         break;
     }
@@ -303,7 +307,8 @@ int main()
 
     sf::Text text("Save the Coral!!", font, 50);
     text.setFillColor(textColor);
-     
+
+    // Initialize molecules
     InitializeMolecules(CarbonDioxide);
     InitializeMolecules(CarbonicAcid);
     InitializeMolecules(Carbonate);
@@ -340,18 +345,21 @@ int main()
                 case sf::Keyboard::W:
                     DecreaseMoleculeCount(CarbonicAcid, 10);
                     break;
+
                 case sf::Keyboard::Z:
                     IncreaseMoleculeCount(Carbonate, 10);
-                    break;                      
-                case sf::Keyboard::X:           
+                    break;
+                case sf::Keyboard::X:
                     DecreaseMoleculeCount(Carbonate, 10);
                     break;
+
                 case sf::Keyboard::D:
                     IncreaseMoleculeCount(BiCarbonate, 10);
                     break;
                 case sf::Keyboard::F:
                     DecreaseMoleculeCount(BiCarbonate, 10);
                     break;
+
                 case sf::Keyboard::E:
                     IncreaseMoleculeCount(CalciumCarbonate, 10);
                     break;
@@ -378,15 +386,15 @@ int main()
         // Draw the molecules
         for (int i = 0; i < currentCarbonDioxideCount; i++)
         {
-            window.draw(CarbonDioxideMolecules[i]);
+            window.draw(carbonDioxideMolecules[i]);
         }
         for (int i = 0; i < currentCarbonicAcidCount; i++)
         {
-            window.draw(CarbonicAcidMolecules[i]);
+            window.draw(carbonicAcidMolecules[i]);
         }
         for (int i = 0; i < currentCarbonateCount; i++)
         {
-            window.draw(CarbonateMolecules[i]);
+            window.draw(carbonateMolecules[i]);
         }
         for (int i = 0; i < currentBiCarbonateCount; i++)
         {
@@ -394,7 +402,7 @@ int main()
         }
         for (int i = 0; i < currentCalciumCarbonateCount; i++)
         {
-            window.draw(CalciumCarbonateMolecules[i]);
+            window.draw(calciumCarbonateMolecules[i]);
         }
 
         // Draw the text
